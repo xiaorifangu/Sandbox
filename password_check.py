56e4a14f7e9ade4c05f9e2b26c9d59680fb7d13a
Li Junlu <junlu.li@my.jcu.edu.au>
2022-04-05 21:41:41 +0800

    Add a password checking function

---
+++ password_check.py
@@ -0,0 +1,11 @@
+
+def is_valid_password(text):
+    return 8 <= len(text) <= 20
+
+def main():
+    new_password = "helloworld"
+    print(f"{new_password} is a valid password?{is_valid_password(new_password)} ")
+
+
+if __name__ == '__main__':
+    main()
