2bf2655f0811c524234f34611369a84d18fe74a7
Li Junlu <junlu.li@my.jcu.edu.au>
2022-04-13 19:27:15 +0800

    Add some comment

---
+++ CP1404 Assessment 1/PC.01.py
@@ -0,0 +1,26 @@
+from typing import List, Tuple
+"""
+Reading Tracker 1.0.
+
+Date: 2022 - 04 - 13.
+
+Author:Li Junlu
+
+Description: This program works as a reading tracker that helps the user
+to know which book is required to read and they haven't done it yet.
+
+Basic logic explanation: My program is highly rely on methods. As you can see 
+methods almost took all the places here while the main program body only took 
+a little..
+
+Precondition: Each line of the csv file where the book information is stored
+has to follow the following format -->  TITLE, AUTHOR, PAGES, IF IT'S REQUIRED 
+For the last one, in this program, we use 'c' to indicate that a book
+is completed and 'r' to indicate that a book is required.
+----------------------------------------------------------------------------
+Violating the precondition will result in this program being crashed.
+----------------------------------------------------------------------------
+
+"""
+
+
