4973927594746e977ac122ade434bc3b162a8c00
Li Junlu <junlu.li@my.jcu.edu.au>
2022-05-11 12:32:05 +0800

    add

---
+++ prac_07/dynamic_labels.py
@@ -0,0 +1,31 @@
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
+
+
+class DynamicWidgetsApp(App):
+    """Main program - Kivy app to demo dynamic widget creation."""
+    status_text = StringProperty()
+
+    def __init__(self, **kwargs):
+        """Construct main app."""
+        super().__init__(**kwargs)
+        # basic data example - dictionary of names: phone numbers
+        # TODO: After running it, add another entry to the dictionary and see how the layout changes
+        self.name_to_phone = {"Bob Brown": "0414144411", "Cat Cyan": "0441411211", "Oren Ochre": "0432123456"}
+
+    def build(self):
+        """Build the Kivy GUI."""
+        self.title = "Dynamic Widgets"
+        self.root = Builder.load_file('dynamic_widgets.kv')
+        self.create_widgets()
+        return self.root
+
+    def clear_all(self):
+        """Clear all of the widgets that are children of the "entries_box" layout widget."""
+        self.root.ids.entries_box.clear_widgets()
+
+
+DynamicWidgetsApp().run()
