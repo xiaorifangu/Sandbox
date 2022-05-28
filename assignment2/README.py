463537df3623d17ac834a4d035ed80afc82fe2f0
Li Junlu <junlu.li@my.jcu.edu.au>
2022-05-28 23:49:13 +0800

    add README.py

---
+++ assignment2/README.py
@@ -0,0 +1,33 @@
+# CP1404 Assignment 2: Reading Tracker 2.0 by Li Junlu
+
+#_Edit this README, replacing this line and above with your own assignment details._
+#_At the end of the project, complete the project reflection below by answering the questions (replace the ... parts)._
+#_Note that to get high marks for this, your reflection should match the "exemplary" description from the rubric:_
+
+#> The project reflection is complete and describes development and learning well, shows careful thought, highlights insights made during code development.
+
+
+## 1. How long did the entire project (assignment 2) take you?
+#It took me about four days.
+#Note: You may like to use the WakaTime plugin, which tracks exactly how long you spend in code. See http://wakatime.com (but note that the free version only has a 7-day history)
+
+## 2. What are you most satisfied with?
+#What I am most satisfied with is that I have completed this assignment with no bug
+
+## 3. What are you least satisfied with?
+#Some code looks weird and not all code is useful
+
+## 4. What worked well in your development process?
+#At the beginning, I was very confused about assignment2 and didn't know how to start. I watched the recording repeatedly to understand what I should do. Recording is really useful
+
+## 5. What about your process could be improved the next time you do a project like this?
+#To be honest I don't know how to improve my code, probably just do a lot of practice
+
+## 6. Describe what learning resources you used and how you used them.
+#I checked a lot of materials and websites, especially CSDN, there are detailed explanations and tutorials on this website, and netizens will also teach you enthusiastically.
+
+## 7. Describe the main challenges or obstacles you faced and how you overcame them.
+#The main challenge was that I didn't know anything about kivy at first, and I found the process from researching materials to completing assignments to be very challenging and painful.
+
+## 8. Briefly describe your experience using classes and if/how they improved your code.
+#Using classes makes my code look more concise and the logic is smoother. Although I don't know its usage very well now, it does benefit me a lot.
