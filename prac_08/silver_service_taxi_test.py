165fff0d8d42fb6089e3138c53e19516bde277f5
Li Junlu <junlu.li@my.jcu.edu.au>
2022-05-31 20:26:41 +0800

    add silver_service_taxi_test.py

---
+++ prac_08/silver_service_taxi_test.py
@@ -0,0 +1,9 @@
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+def main():
+    taxi = SilverServiceTaxi("Test Fancy taxi", 100, 2)
+    taxi.drive(18)
+    print(taxi)
+    print(taxi.get_fare())
+
+main()
